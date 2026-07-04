b6196a5ce932158fe19265ade4987914187994f3
HelliusYunikaHartono <helliushartono8@gmail.com>
2026-07-05 00:20:17 +0700

    ganti admin seader ke kader123

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -11,9 +11,9 @@ class AdminSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'nama_anak' => 'admin123',
+            'nama_anak' => 'kader123',
             'nik_anak' => null,
-            'password' => Hash::make('admin123'),
+            'password' => Hash::make('kader123'),
             'role' => 'admin',
             'created_at' => now(),
             'updated_at' => now(),
